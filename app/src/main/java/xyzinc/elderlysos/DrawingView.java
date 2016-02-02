ba338d1395f3e2f8fbe93cb8e65ddbd5d485a690
XYLau <lauxingyi@hotmail.com>
2016-02-02 22:07:28 +0800

    setup

---
+++ app/src/main/java/xyzinc/elderlysos/DrawingView.java
@@ -0,0 +1,21 @@
+package xyzinc.elderlysos;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by XY Lau on 18/1/2016.
+ */
+public class DrawingView extends View {
+    private static final String TAG = "DrawingView";
+
+
+    public DrawingView(Context context, AttributeSet attributeSet){
+        super (context, attributeSet);
+        setUpDrawing();
+    }
+    public void setUpDrawing(){
+            
+    }
+}
